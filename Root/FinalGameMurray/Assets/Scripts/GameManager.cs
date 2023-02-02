62b4ce5e3396a1386f66aec8639ad87fef3f26c2
Skyfall1235 <113722636+Skyfall1235@users.noreply.github.com>
2023-02-02 17:20:53 -0500

    added second level and win condition with scene

--- Root/FinalGameMurray/Assets/Scripts/GameManager.cs
+++ Root/FinalGameMurray/Assets/Scripts/GameManager.cs
@@ -18,7 +18,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] int level;
     [SerializeField] float localTime;
     [SerializeField] float globalTime;
-    private string displayLeveltime;
+    [SerializeField] private string displayLeveltime;
 
 
     public int[] potions = new int[3] { 0, 0, 0 };
@@ -27,15 +27,17 @@ public class GameManager : MonoBehaviour
 
     [SerializeField] GameObject gameOverPanel;
     [SerializeField] GameObject youWinPanel;
-    [SerializeField] TextMeshProUGUI UIText;
+    [SerializeField] TextMeshProUGUI coinText;
+    [SerializeField] TextMeshProUGUI timeText;
     
 
     [SerializeField] private bool allowInput;
+    bool timerAllowed = true;
     
     // Start is called before the first frame update
     void Start()
     {
-
+        playerInfo.currentLevel = playerInfo.currentLevel + 1;
     }
 
     // Update is called once per frame
@@ -44,21 +46,25 @@ public class GameManager : MonoBehaviour
         health = Player.GetComponent<BallPlayerController>().health;
         CheckLevelWin();
         PotionManagement();
-        KeepLocalTime();
-        KeepGlobalTime(playerInfo.currentLevel)
+        if(timerAllowed == true)
+        {
+            KeepLocalTime();
+            KeepGlobalTime(playerInfo.currentLevel);
+        }
+        timeText.text = displayLeveltime;
+        ChangeColorOfText();
     }
-    //public void HandleGameOver()
-    //{
-    //    gameOverPanel.SetActive(true);
-    //    allowInput = false;
-    //}
+
+    #region timekeeping
+    int minutes = 0;
+    
     void KeepLocalTime()
     {
-        localTime = localTime + Time.deltaTime;
-        globalTime = Time.deltaTime;
-
-        int minutes = 0;
         int seconds = (int)localTime;
+        localTime = localTime + Time.deltaTime;
+        globalTime = globalTime + Time.deltaTime;
+
+        
         //if thers s60 seconds passed, increment the minute and restart
         if (localTime >= 60)
         {
@@ -66,11 +72,26 @@ public class GameManager : MonoBehaviour
             minutes++;
         }
         displayLeveltime = $"{minutes}m, {seconds}s";
+
+    }
+    string ConvertIntTimeToString(int inputAmountOfSeconds)
+    {
+        int minutes;
+        int seconds;
+
+        seconds = inputAmountOfSeconds % 60;
+        minutes = seconds / 60;
+
+
+        return $"{minutes}m, {seconds}s";
     }
     void KeepGlobalTime(int currentLevel)
     {
-        playerInfo.
+        //playerInfo.level1 +level2 +level3 time
+        //diplay time
     }
+    #endregion
+    #region Crucial Functions
     public void CheckLevelWin()
     {
         //if (coins == 10)
@@ -79,6 +100,27 @@ public class GameManager : MonoBehaviour
         //    allowInput = false;
         //}
     }
+
+    public void SaveData(int currentLevel)
+    {
+        playerInfo.currentLevel = health;
+        playerInfo.currentLevel = currentLevel;
+        if(currentLevel == 1)
+        {
+            playerInfo.levelOneTime = globalTime; 
+        }
+        else if(currentLevel == 2)
+        {
+            playerInfo.levelTwoTime = globalTime;
+        }
+        else if(currentLevel == 3)
+        {
+            playerInfo.levelThreeTime = globalTime;
+        }
+
+
+        playerInfo.totalTime = playerInfo.levelOneTime + playerInfo.levelTwoTime + playerInfo.levelThreeTime;
+    }
     public void Restart()
     {
         //set potions counts to 0
@@ -86,7 +128,17 @@ public class GameManager : MonoBehaviour
     }
     public void UpdateData(int hp)
     {
-        UIText.text = ($"Player HP: {hp}\n Coins Collected:{coins}");
+        coinText.text = ($"Player HP: {hp}\n Coins Collected:{coins}");
+    }
+    #endregion
+    void ChangeColorOfText()
+    {
+        if (coins >= 10)
+        {
+            timeText.color = Color.red;
+            timerAllowed = false;
+        }
+        
     }
     void PotionManagement()
     {
@@ -122,10 +174,10 @@ public class GameManager : MonoBehaviour
             }
         }
     }
-    void ShiftScene(int _levelArrayIdx)
+    public void ShiftScene(int _levelArrayIdx)
     {
-        //if ()
-        string[] levels = new string[] { "DesignLevelOne", "DesignLevelTwo", "DesignLevelThree" };
+        
+        string[] levels = new string[] { "DesignLevelOne", "DesignLevelTwo", "WinScene" };
 
         SceneManager.LoadScene(levels[_levelArrayIdx]);
     }
--- Root/FinalGameMurray/Assets/Scripts/NextScenetrigger.cs
+++ Root/FinalGameMurray/Assets/Scripts/NextScenetrigger.cs
@@ -8,23 +8,27 @@ public class NextScenetrigger : MonoBehaviour
     
     [SerializeField] private int levelToGoTo;
     [SerializeField] private GameObject gameManager;
+    [SerializeField] private ScriptableObjectCreation playerData;
     // Start is called before the first frame update
     void Start()
     {
-        
+        levelToGoTo = playerData.currentLevel + 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
     
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player" && gameManager.GetComponent<GameManager>().coins == 10)
         {
-            ShiftScene(levelToGoTo);
+            //save the data first
+            gameManager.GetComponent<GameManager>().SaveData(playerData.currentLevel);
+            //send us over to thew next scene
+            gameManager.GetComponent<GameManager>().ShiftScene(levelToGoTo);
         }
     }
 }
